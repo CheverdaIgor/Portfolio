7669febe1137bfad21d08741d367357b2b65331a
Igor Cheverda <cheverda@i.ua>
2023-10-30 00:17:07 +0200

    Added portfolio files

--- 10-Cypress/OrangeHRM/spec.cy.js
+++ 10-Cypress/OrangeHRM/spec.cy.js
@@ -2,28 +2,92 @@ import { loginUser } from './loginPage.cy';
 
 describe('Login and Navigation Tests', () => {
     beforeEach(() => {
-        loginUser("Admin", "admin123");
-        cy.window().then((win) => {
-            cy.stub(win.console, 'error').as('consoleError');
-        });
+      cy.window().then((win) => {
+        cy.stub(win.console, 'error').as('consoleError');
+      });
+      loginUser("Admin", "admin123");
     });
 
     it('Navigates to Landing Page', () => {
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
-        cy.get('a[href="/web/index.php/pim/viewMyDetails"]').click();        
-        //cy.wait(1000);
+        //cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+        //cy.get('a[href="/web/index.php/pim/viewMyDetails"]').click();
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7');
+        cy.wait(1000);
+        
+        let data = 'test';
+        let date = '2000-10-10';
+        let isCheckboxChecked, isRadioChecked;
 
-        //cy.get('input.oxd-input.oxd-input--active')
         cy.get('div[class="oxd-form-row"] > div')
-        .find('input')
-        .each(($input) => {
+        .find('input').as('finputTest')
+        .should('have.length', 15)
+        .each(($input, index) => {
+
           if ($input.attr('placeholder') === 'yyyy-mm-dd') {
-            cy.wrap($input).clear({ force: true }).type('2000-10-10', { force: true });
-          } else if ($input.attr('type') === 'radio' || $input.attr('type') === 'checkbox') {
-            cy.wrap($input).check({ force: true });
+            cy.wrap($input)
+            .clear({ force: true })
+            .type(date, { force: true });
+
+          } else if ($input.attr('type') === 'radio') {
+            if (!$input.is(':checked')) {
+              cy.wrap($input)
+              .check({ force: true });
+            } isRadioChecked = index;
+          
+          } else if ($input.attr('type') === 'checkbox') {
+            if (!$input.is(':checked')) {
+              cy.wrap($input)
+              .click({ force: true });
+            } isCheckboxChecked = index;
+          
           } else {
-            cy.wrap($input).clear({ force: true }).type('test', { force: true });
-          }
+            cy.wrap($input)
+            .clear({ force: true })
+            .type(data, { force: true });
+            }
+          });
+
+        cy.get('.oxd-select-text-input')
+        .should('have.length', 3)
+        .each(($dropdown) => {
+          cy.wrap($dropdown)
+          .type('o', { force: true })
+          .click();
         });
-    });
+          
+        //cy.get('[class="oxd-radio-input oxd-radio-input--active --label-right oxd-radio-input"]:first').click();
+        //cy.get('[class="oxd-icon bi-check oxd-checkbox-input-icon"]:first').click();
+        
+        cy.get('@finputTest')
+        .should('have.length', 15)
+        .each(($input, index) => {
+          
+          if ($input.attr('placeholder') === 'yyyy-mm-dd') {
+            cy.wrap($input)
+            .should('have.value', date);
+
+          } else if ($input.attr('type') === 'radio') {
+              if (index === isRadioChecked) {
+                cy.wrap($input)
+                .should('be.checked');
+              } else {
+                cy.wrap($input)
+                .should('not.be.checked');
+              }
+          
+          } else if ($input.attr('type') === 'checkbox') {
+              if (index === isCheckboxChecked) {
+                cy.wrap($input)
+                .should('be.checked');
+              } else {
+                cy.wrap($input)
+                .should('not.be.checked');
+              }
+          
+          } else {
+            cy.wrap($input)
+            .should('have.value', data);
+            }
+          });
+      });
 });
